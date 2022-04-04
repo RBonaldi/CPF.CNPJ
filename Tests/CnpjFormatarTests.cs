f20e984f503473daed18a57515f270fc3465d4c1
Rafael Bonaldi Simoes <trc6016997@unimedbh.com.br>
2022-04-04 00:00:36 -0300

    Versão dotnet

---
+++ Tests/CnpjFormatarTests.cs
@@ -0,0 +1,18 @@
+﻿namespace CnpjLibrary.Tests
+{
+    using CpfCnpjLibrary;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CnpjFormatarTests
+    {
+        [TestMethod]
+        public void Valid()
+        {
+            Assert.AreEqual("67.419.649/0001-12", Cnpj.FormatarComPontuacao("67.419.649/0001-12"));
+            Assert.AreEqual("67419649000112", Cnpj.FormatarSemPontuacao("67.419.649/0001-12"));
+            Assert.AreEqual("02.287.987/0001-00", Cnpj.FormatarComPontuacao("2.287.987/0001-00"));
+            Assert.AreEqual("02287987000100", Cnpj.FormatarSemPontuacao("2.287.987/0001-00"));
+        }
+    }
+}
---
+++ Tests/CnpjValidarTests.cs
@@ -0,0 +1,20 @@
+﻿namespace CnpjLibrary.Test
+{
+    using CpfCnpjLibrary;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CnpjValidarTests
+    {
+        [TestMethod]
+        public void ValidWithMask()
+        {
+            Assert.IsTrue(Cnpj.Validar("67.419.649/0001-12"));
+            Assert.IsTrue(Cnpj.Validar("67419649000112"));
+            Assert.IsTrue(Cnpj.Validar("02.944.203/0001-61"));
+            Assert.IsTrue(Cnpj.Validar("2.944.203/0001-61"));
+            Assert.IsTrue(Cnpj.Validar("02089275000179"));
+            Assert.IsTrue(Cnpj.Validar("2089275000179"));
+        }
+    }
+}
